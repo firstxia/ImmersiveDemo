5826132b2b9af7e94a0b0480c6b67ae82957022c
Xialm <firstxia168@163.com>
2016-11-23 14:38:42 +0800

    全屏,设置状态栏和导航栏为透明

--- app/src/main/java/com/xialm/immersivedemo/MainActivity.java
+++ app/src/main/java/com/xialm/immersivedemo/MainActivity.java
@@ -19,9 +19,13 @@ public class MainActivity extends AppCompatActivity {
             View decorView = getWindow().getDecorView(); //获取当前界面的decorview
 //            int option = View.SYSTEM_UI_FLAG_FULLSCREEN; //全屏,隐藏状态栏(带LAYOUT的不行),导航栏不会隐藏
 //            int option=View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;  //让应用的主体内容占用系统状态栏的空间
-            int option=View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION; //全屏,隐藏状态栏和导航栏
+//            int option=View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION; //全屏,隐藏状态栏和导航栏
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
             decorView.setSystemUiVisibility(option);
-//            getWindow().setStatusBarColor(Color.TRANSPARENT); //设置状态栏为透明色
+            getWindow().setStatusBarColor(Color.TRANSPARENT); //设置状态栏为透明色
+            getWindow().setNavigationBarColor(Color.TRANSPARENT); // 设置导航栏为透明色
         }
 
         // 根据Android的设计,actionbar是不应该独立于状态栏而单独显示的,如果状态栏隐藏了,也要隐藏actionbar
